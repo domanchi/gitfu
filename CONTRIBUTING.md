5263e86973b1aefa1696e033f5d345bb858e1bbc
Aaron Loo <admin@aaronloo.com>
2021-04-20 07:47:47 -0700

    bug fix: gitfu init is now compatible with pyenv

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -19,12 +19,18 @@ TODO
 
 ### Manual Testing of Installing from Python Wheel
 
+This is helpful to test the bash shim (when we can't use the editable pip install to check
+for path manipulations).
+
 ```bash
+# Use this, if the next command doesn't know how to build wheels.
+$ pip install wheel
+
 # First, we need to build a wheel
 $ python setup.py bdist_wheel --universal
 
 # Then, we can install from that wheel
-$ pip install --only-binary :all: --no-cache -f file:///`pwd`/dist gitfu
+$ pip install --force-reinstall --only-binary :all: --no-cache -f file:///`pwd`/dist gitfu
 
 # Clean up after ourselves
 rm -r build dist
--- gitfu/__main__.py
+++ gitfu/__main__.py
@@ -1,4 +1,5 @@
 import os
+import re
 import sys
 import textwrap
 
@@ -27,14 +28,67 @@ def run() -> int:
 
 
 def get_bash_shim() -> str:
+    # NOTE: Especially with pyenv, gitfu will be installed to the respective python environment.
+    # However, this may not be in the user's $PATH (since it would probably be in some directory
+    # like ~/pyenv/versions/<version>/bin/gitfu). Furthermore, we don't want to necessarily add
+    # this entire directory to the path (as it would override other python shims).
+    #
+    # As such, we find the location that `gitfu` is installed into, and figure out the binary
+    # location ourselves. In this way, not only will we not need to do $PATH manipulations, but
+    # it will also work when we change python versions with pyenv.
+    #
+    # NOTE: We assume that the binaries will be installed to <prefix>/bin/*, and the source files
+    # will be installed to <prefix>/lib/python3.\d+/site-packages/gitfu/.
+    python_regex = re.compile(r'(?P<prefix>.*?)\/python3\.\d+$')
+    directory = os.path.dirname(__file__)
+    while not python_regex.match(directory):
+        new_directory, tail = os.path.split(directory)
+        if new_directory == directory:
+            break
+
+        directory = new_directory
+
+    if directory == '/':
+        # TODO: This probably means it's invoked through other means.
+        raise NotImplementedError(
+            'This python install does not follow regular conventions. '
+            'Are you using an editable installation? (e.g. `pip install -e`)',
+        )
+
+    bin_directory = os.path.realpath(os.path.join(directory, '../../bin'))
+
     # NOTE: We use a function here, so that it's easy to uninstall (i.e. `unset git`),
     # yet it doesn't interfere with our PATH. As such, it is only a user-based shim,
     # and (hopefully) won't affect any other scripts that depend on `git`.
-    return textwrap.dedent("""
-        function git {
-            gitfu run "$@"
-        }
-    """)[1:-1]
+    output = []
+    output.append(
+        textwrap.dedent(f"""
+            function git {{
+                {bin_directory}/gitfu run "$@"
+            }}
+        """)[1:-1],
+    )
+
+    standalone_script_names = [
+        os.path.splitext(item)[0].replace('_', '-')
+        for item in os.listdir(
+            os.path.join(
+                os.path.dirname(__file__),
+                'standalone',
+            ),
+        )
+        if not item.startswith('_')
+    ]
+    output.extend([
+        textwrap.dedent(f"""
+            function {name} {{
+                {bin_directory}/{name} "$@"
+            }}
+        """)[1:-1]
+        for name in standalone_script_names
+    ])
+
+    return '\n\n'.join(output)
 
 
 def show_help() -> str:
