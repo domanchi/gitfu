6db2bc79157a2b695aef88080693717fdf946aae
Aaron Loo <admin@aaronloo.com>
2022-12-24 08:03:49 -0800

    bug fix: use --relative flag instead for add_git_staged_files

--- gitfu/standalone/add_git_staged_files.py
+++ gitfu/standalone/add_git_staged_files.py
@@ -7,25 +7,14 @@ from ..core import git
 
 def main(*argv: str) -> None:
     staged_files = git.run(
-        'diff', '--staged', '--name-only',
+        'diff', '--staged', '--name-only', '--relative',
         '--diff-filter=ARM',
     ).splitlines()
     if not staged_files:
         # If no staged files, add all tracked files.
         git.run('add', '-u')
     else:
-        # NOTE: If you run this script in a non-root directory, `staged_files` will show
-        # paths relative to the git root, but we won't be able to add it directly. As such,
-        # compute the prefix to the current directory, and we can remove this prefix accordingly.
-        root = git.run('rev-parse', '--show-toplevel')
-        prefix = subprocess.check_output(
-            ['realpath', '--relative-to', root, '.'],
-        ).decode().rstrip()
-        if prefix == '.':
-            prefix = ''
-
-        files_to_add = [filename[len(prefix):] for filename in staged_files]
-        git.run('add', *files_to_add)
+        git.run('add', *staged_files)
 
 
 if __name__ == '__main__':
